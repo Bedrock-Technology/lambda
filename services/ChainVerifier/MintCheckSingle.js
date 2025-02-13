09b4717f0bd04fccc5bd7fd87995223f56e846a5
f2c3ed6a <163963684+f2c3ed6a@users.noreply.github.com>
2025-02-13 14:32:13 +0800

    feat: add ChainVerifier/MintCheckSingle & ChainVerifier/MintCheckBoth

---
+++ services/ChainVerifier/MintCheckSingle.js
@@ -0,0 +1,44 @@
+// var rune_api_base = 'http://host.docker.internal:8580' // beta
+var rune_api_base = 'http://host.docker.internal:8570' // prod
+
+function firstOr(x, defaultValue) {
+    if (x && x.length > 0) {
+        return x[0]
+    }
+    return defaultValue
+}
+
+function getAmountByFunc(funcName, addr, start, end) {
+    var payload = {
+        func_name: funcName,
+        params: JSON.stringify({
+            user: addr,
+            start_time: start,
+            end_time: end,
+        })
+    }
+
+    var resp = fetch(rune_api_base + '/dsn/execsql', {
+        method: 'POST',
+        body: JSON.stringify(payload)
+    })
+
+    var respObj = JSON.parse(resp.body)
+    var mintedAmount = 0
+    if (respObj.data && respObj.data[0]) {
+        mintedAmount = Number(respObj.data[0].total_amount)
+    }
+    return mintedAmount
+}
+
+var addr = firstOr(req.query.address, '')
+var start = firstOr(req.query.from, 0)
+var end = firstOr(req.query.to, 0)
+var amountLimit = firstOr(req.query.amount, 0)
+
+var brBTCAmount = getAmountByFunc('FUNGetUserMintedBrBtcAmountALLCHAIN', addr, start, end)
+var uniBTCAmount = getAmountByFunc('FUNGetUserMintedUniBtcAmountALLCHAIN', addr, start, end)
+
+JSON.stringify({
+    result: (brBTCAmount >= amountLimit) || (uniBTCAmount >= amountLimit)
+})
