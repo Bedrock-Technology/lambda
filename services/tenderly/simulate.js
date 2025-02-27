5532fa59302de6331ba22af368a8974ffad8bcc4
f2c3ed6a <163963684+f2c3ed6a@users.noreply.github.com>
2025-02-27 18:24:04 +0800

    feat: add tenderly api

---
+++ services/tenderly/simulate.js
@@ -0,0 +1,41 @@
+var apiKey = ''
+
+var obj = JSON.parse(req.body)
+
+var simulateResp = fetch('https://api.tenderly.co/api/v1/account/me/project/project/simulate', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'X-Access-Key': apiKey,
+    },
+    body: JSON.stringify({
+        network_id: Number(obj.chain_id),
+        block_number: Number(obj.block_number),
+        from: obj.from,
+        to: obj.to,
+        value: obj.value,
+        data: obj.data,
+        save: true,
+        save_if_fails: true,
+    }),
+})
+
+var simulateRespObj = JSON.parse(simulateResp.body)
+var simId = simulateRespObj.simulation.id
+
+var shareResp = fetch('https://api.tenderly.co/api/v1/account/me/project/project/simulations/' + simId + '/share', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'X-Access-Key': apiKey,
+    },
+    body: '',
+})
+_ = shareResp
+
+var outputUrl = 'https://www.tdly.co/shared/simulation/' + simId
+
+JSON.stringify({
+    output_url: outputUrl,
+    simulation_id: simId,
+})
